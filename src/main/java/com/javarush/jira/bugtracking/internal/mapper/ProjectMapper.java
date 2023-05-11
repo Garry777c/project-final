e8a8e55a329ca98241d71c7b5b81c34ed12b4855
Igor Golovko <golovko1978@gmail.com>
2023-05-11 10:30:24 -0600

    removed vk, yandex, added @Component to three mappers

--- src/main/java/com/javarush/jira/bugtracking/internal/mapper/ProjectMapper.java
+++ src/main/java/com/javarush/jira/bugtracking/internal/mapper/ProjectMapper.java
@@ -5,10 +5,12 @@ import com.javarush.jira.bugtracking.to.ProjectTo;
 import com.javarush.jira.common.BaseMapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 import java.util.List;
 
+@Component
 @Mapper(componentModel = "spring")
 public interface ProjectMapper extends BaseMapper<Project, ProjectTo> {
 
